220f2ab287eb837c3070ef6ad86a11c6ac9bcf95
Roman31X <die.haa65@gmail.com>
2024-06-05 15:05:00 -0500

    Modelo persona agregada

--- src/main/java/org/example/aplicacion/AplicacionListarPersona.java
+++ src/main/java/org/example/aplicacion/AplicacionListarPersona.java
@@ -1,2 +1,7 @@
-package org.example.aplicacion;public class AplicacionListarPersona {
+package org.example.aplicacion;
+
+public class AplicacionListarPersona {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
 }
